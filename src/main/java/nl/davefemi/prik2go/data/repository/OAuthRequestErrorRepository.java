ed43d86b035be4c48fbaa330935ec2f5d3a087aa
davefemi <david.ojo@live.com>
2026-02-27 00:33:41 +0100

    Refactor OAuthService login validation

--- src/main/java/nl/davefemi/prik2go/data/repository/OAuthRequestErrorRepository.java
+++ src/main/java/nl/davefemi/prik2go/data/repository/OAuthRequestErrorRepository.java
@@ -1,5 +1,6 @@
 package nl.davefemi.prik2go.data.repository;
 
+import jakarta.transaction.Transactional;
 import nl.davefemi.prik2go.data.entity.OAuthRequestEntity;
 import nl.davefemi.prik2go.data.entity.OAuthRequestErrorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface OAuthRequestErrorRepository extends JpaRepository <OAuthRequestErrorEntity, Long> {
 
+    @Transactional
     @Query("SELECT r FROM OAuthRequestErrorEntity r WHERE r.requestId= :requestId")
     OAuthRequestErrorEntity findByRequestId(@Param("requestId") OAuthRequestEntity requestId);
 }
