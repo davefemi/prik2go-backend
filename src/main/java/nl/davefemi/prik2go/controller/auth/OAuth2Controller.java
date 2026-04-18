3c75c2669cbc7ed9025dcfb0dca4cf6e15e371fe
davefemi <david.ojo@live.com>
2026-04-18 22:26:34 +0200

    Refactors

--- src/main/java/nl/davefemi/prik2go/controller/auth/OAuth2Controller.java
+++ src/main/java/nl/davefemi/prik2go/controller/auth/OAuth2Controller.java
@@ -16,7 +16,6 @@ import nl.davefemi.prik2go.service.auth.OAuth2Service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.security.Principal;
 import java.util.Optional;
